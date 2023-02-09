3adbc957006d3f9ccd4dc84ee41067a84eb9ecb8
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2023-02-10 03:28:28 +0400

    Исправил отображение рабочих часов в объекте месяца

--- helpers/generateMonth.js
+++ helpers/generateMonth.js
@@ -15,7 +15,7 @@ const generateMonth = (year, month) => {
     workingDays,
     notWorkingDays: getDaysCount(year, month) - workingDays,
     shortDays: countShortDays(year, correctMonth),
-    workHours: countWorkingHours(year, correctMonth),
+    workHours: countWorkingHours(year, month),
   };
 
   return obj;
