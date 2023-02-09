21efdcb56134e3e3bc6a32bf2302e4e4954f4554
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2023-02-09 21:36:54 +0400

    Добавлено поле notWorkingDays для отображения количества нерабочих дней в объекте месяца

--- helpers/generateMonth.js
+++ helpers/generateMonth.js
@@ -1,13 +1,16 @@
 const { countWorkingDays } = require('./countWorkingDays');
+const { getDaysCount } = require('./getDaysCount');
 
 const generateMonth = (year, month) => {
   const correctMonth = month - 1;
+  const workingDays = countWorkingDays(year, correctMonth);
 
   const monthName = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(new Date(year, correctMonth, 1));
   const obj = {
     name: monthName,
     id: correctMonth,
-    workingDays: countWorkingDays(year, correctMonth),
+    workingDays,
+    notWorkingDays: getDaysCount(year, month) - workingDays,
   };
 
   return obj;
--- helpers/generateMonths.js
+++ helpers/generateMonths.js
@@ -1,13 +1,17 @@
 const { countWorkingDays } = require('./countWorkingDays');
+const { getDaysCount } = require('./getDaysCount');
 
 const generateMonths = (year) => {
   const months = [];
   for (let month = 0; month < 12; month++) {
     const monthName = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(new Date(year, month, 1));
+    const workingDays = countWorkingDays(year, month);
+
     const obj = {
       name: monthName,
       id: month,
-      workingDays: countWorkingDays(year, month),
+      workingDays,
+      notWorkingDays: getDaysCount(year, month + 1) - workingDays,
     };
     months.push(obj);
   }
