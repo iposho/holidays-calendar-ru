3f321a8b6dd8375f7c673e3661b72d59c7cefc0e
Pavel Kuzyakin <p.kuzyakin@eapteka.ru>
2024-03-05 14:44:06 +0400

    [fix] Исправлено падение календаря за 2023 год

--- data/workingHolidays.js
+++ data/workingHolidays.js
@@ -1,10 +1,15 @@
 const { createDateString } = require('../helpers/createDateString');
 
+const wh2023 = () => [
+
+];
+
 const wh2024 = (year = 2024) => [
   createDateString(year, 10, 2, 'День народного единства'),
   createDateString(year, 11, 28, 'Новый год'),
 ];
 
 module.exports = {
+  wh2023,
   wh2024,
 };
