2cd76c35d37d9442f804a7b0d1be2cc19f8709f4
Pavel Kuzyakin <iposho@gmail.com>
2024-04-30 22:34:01 +0400

    [feat] Яндекс.Метрика
    - Добавлена инициализация Яндекс.Метрики и dotenv

--- helpers/createMainPage.js
+++ helpers/createMainPage.js
@@ -1,6 +1,11 @@
 const { marked } = require('marked');
 const path = require('path');
 const fs = require('fs');
+const dotenv = require('dotenv');
+
+const env = process.env.NODE_ENV || 'development';
+const envFile = env === 'production' ? '.env' : '.env.local';
+dotenv.config({ path: envFile });
 
 const head = `
   <head>
@@ -34,11 +39,40 @@ const styles = `
   </style>
 `;
 
+const yandexMetrikaId = process.env.YANDEX_METRIKA_ID;
+const yandexMetrika = yandexMetrikaId
+  ? `
+    <script type="text/javascript">
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(${yandexMetrikaId}, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+      });
+    </script>
+    <noscript>
+      <div>
+        <img src="https://mc.yandex.ru/watch/97168303" style="position:absolute; left:-9999px;" alt="" />
+      </div>
+    </noscript>
+  `
+  : '';
+
 const createMainPage = (res) => {
   const readmePath = path.join(__dirname, '../README.md');
   const file = fs.readFileSync(readmePath, 'utf8');
   res.set('Content-Type', 'text/html');
-  res.send(Buffer.from(head) + Buffer.from(styles) + marked(file.toString()));
+  res.send(
+    Buffer.from(head)
+    + Buffer.from(styles)
+    + marked(file.toString())
+    + Buffer.from(yandexMetrika),
+  );
 };
 
 module.exports = {
