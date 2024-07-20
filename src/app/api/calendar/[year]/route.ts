f0adb256115aa7309008f5e84f1d25a46b554624
Pavel Kuzyakin <iposho@gmail.com>
2024-07-20 05:11:06 +0400

    [feat] Календарь на 2025 год
    - добавлен проект календаря на 2025 год

--- src/app/api/calendar/[year]/route.ts
+++ src/app/api/calendar/[year]/route.ts
@@ -2,11 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import { generateData } from '@/helpers/generateData';
 import { isNotCorrectYear } from '@/helpers/isNotCorrect';
 import { getErrorMessages } from '@/helpers/getErrorMessages';
+import { availableYears } from '@/helpers/availableYears';
 
 const data = generateData();
 
 export async function generateStaticParams() {
-  const years = [2023, 2024];
+  const years = availableYears();
+  console.log({ years });
   return years.map((year) => ({ year: year.toString() }));
 }
 
--- src/data/workingHolidays.ts
+++ src/data/workingHolidays.ts
@@ -8,10 +8,14 @@ interface WorkingHoliday {
 type WorkingHolidaysFunction = (year?: number) => WorkingHoliday[];
 
 export const wh2023: WorkingHolidaysFunction = () => [
-  // Рабочие праздничных дней не было в 2023 году
+  // Рабочих выходных дней в 2023 году нет
 ];
 
 export const wh2024: WorkingHolidaysFunction = (year = 2024) => [
   createDateString(year, 10, 2, 'День народного единства'),
   createDateString(year, 11, 28, 'Новый год'),
+];
+
+export const wh2025: WorkingHolidaysFunction = () => [
+  // Рабочих выходных дней в 2025 году нет
 ];
