79d2ba7539709b057e9a848b8ec858d5f102e248
Pavel Kuzyakin <iposho@gmail.com>
2024-07-20 05:42:52 +0400

    (fix) Заголовок главной страницы
    Signed-off-by: Pavel Kuzyakin <iposho@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import { ReactNode } from 'react';
+import { Metadata } from 'next';
 
 import '../styles/global.scss';
 
 interface LayoutProps {
   children: ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: 'Производственный календарь',
+};
 
 function Layout({ children }: LayoutProps) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-
 import { getReadmeContent } from '@/helpers/getReadmeContent';
 
 const HomePage = async () => {
@@ -7,9 +5,6 @@ const HomePage = async () => {
 
   return (
     <div className="container mx-auto p-4">
-      <Head>
-        <title>Производственный календарь</title>
-      </Head>
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
