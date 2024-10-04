6339f4f14b4f8a3b264a177671af4e53ffd0da8b
Pavel Kuzyakin <iposho@gmail.com>
2024-10-04 21:22:10 +0400

    [feat] Календарь на 2025
    http://government.ru/news/52895/
    Signed-off-by: Pavel Kuzyakin <iposho@gmail.com>

--- src/data/holidays.ts
+++ src/data/holidays.ts
@@ -58,14 +58,19 @@ export const h2025: HolidaysFunction = (year = 2025) => [
   createDateString(year, 0, 6, 'Новый год'),
   createDateString(year, 0, 7, 'Новый год'),
   createDateString(year, 0, 8, 'Новый год'),
-  createDateString(year, 1, 24, 'День защитника Отечества'),
-  createDateString(year, 2, 10, 'Международный женский день'),
+  createDateString(year, 1, 22, 'День защитника Отечества'),
+  createDateString(year, 1, 23, 'День защитника Отечества'),
+  createDateString(year, 2, 8, 'Международный женский день'),
+  createDateString(year, 2, 9, 'Международный женский день'),
   createDateString(year, 4, 1, 'День Труда'),
   createDateString(year, 4, 2, 'День Труда'),
+  createDateString(year, 4, 8, 'День Победы'),
   createDateString(year, 4, 9, 'День Победы'),
   createDateString(year, 5, 12, 'День России'),
   createDateString(year, 5, 13, 'День России'),
+  createDateString(year, 10, 3, 'День народного единства'),
   createDateString(year, 10, 4, 'День народного единства'),
+  createDateString(year, 11, 31, 'Новый год'),
 ];
 
 export const holidays: Record<string, HolidaysFunction> = {
