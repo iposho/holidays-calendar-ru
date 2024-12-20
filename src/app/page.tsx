c986f3c9cf955bc4ec0091edb82c5e7fe0a4a9ce
Pavel Kuzyakin <40692570+iposho@users.noreply.github.com>
2024-12-20 17:02:42 +0400

    feat: на главной странице рендерим README.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
 import { fetchLocalReadme } from '@/utils/ReadmeFetcher';
 
+import packageJson from '../../package.json';
+
 import css from './page.module.scss';
 
 export default async function HomePage() {
@@ -26,7 +28,7 @@ export default async function HomePage() {
             components={{
               // eslint-disable-next-line react/no-unstable-nested-components
               img: ({ node, ...props }) => (
-                <img {...props} alt={props.alt || 'Изображение'} />
+                <img {...props} alt={props.alt || ''} />
               ),
             }}
           >
@@ -42,7 +44,16 @@ export default async function HomePage() {
             {new Date().getFullYear()}
           </span>
           <span>
-            <a href="https://kuzyak.in">Павел Кузякин</a>
+            <a href="https://kuzyak.in">
+              Павел Кузякин
+            </a>
+          </span>
+        </div>
+        <div className={css.version}>
+          <span>
+            <a href="https://github.com/iposho/holidays-calendar-ru">
+              {packageJson?.version}
+            </a>
           </span>
         </div>
       </footer>
