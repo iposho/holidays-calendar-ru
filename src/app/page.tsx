2e7e86e741a0d6d3f0aaf63785a7f0908305c75a
Pavel Kuzyakin <40692570+iposho@users.noreply.github.com>
2024-12-20 05:33:24 +0400

    feat: на главной странице рендерим README.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,45 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+import { fetchLocalReadme } from '@/utils/ReadmeFetcher';
 
 import css from './page.module.scss';
 
-export default function Homepage() {
+export default async function HomePage() {
+  let markdown = '';
+
+  try {
+    markdown = await fetchLocalReadme();
+  } catch (error) {
+    markdown = 'Не удалось загрузить содержимое README.md.';
+  }
+
   return (
     <div className={css.main}>
-      <header className={css.header}>
-        <h1 className={css.title}>Производственный календарь 🇷🇺</h1>
-      </header>
 
       <div className={css.content}>
         <div className={css.description}>
-          <p>
-            Производственные календари РФ (2023—2025) в формате JSON. Простой API для получения данных.
-          </p>
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeHighlight]}
+            components={{
+              // eslint-disable-next-line react/no-unstable-nested-components
+              img: ({ node, ...props }) => (
+                <img {...props} alt={props.alt || 'Изображение'} />
+              ),
+            }}
+          >
+            {markdown}
+          </ReactMarkdown>
         </div>
-
-        <section className={css.links}>
-          <a className={css.link} href="https://github.com/iposho/holidays-calendar-ru?tab=readme-ov-file#как-получить">📔 Документация</a>
-        </section>
       </div>
+
       <footer className={css.footer}>
-        <div className={css.badges}>
-          <img src="https://img.shields.io/uptimerobot/ratio/7/m797301234-a06cf748375429b73d2bee31" alt="Uptinerobot" />
-          <img src="https://vercelbadge.vercel.app/api/iposho/holidays-calendar-ru?style=flat" alt="Vercel" />
-          <img src="https://img.shields.io/github/languages/code-size/iposho/holidays-calendar-ru" alt="GitHub Size" />
-          <img src="https://img.shields.io/github/last-commit/iposho/holidays-calendar-ru" alt="Last Commit" />
-          <img src="https://img.shields.io/github/license/iposho/holidays-calendar-ru" alt="MIT LICENSE" />
-        </div>
         <div className={css.copyright}>
           <span>
-            {`© 2023...${new Date().getFullYear()}`}
+            © 2023...
+            {new Date().getFullYear()}
           </span>
           <span>
             <a href="https://kuzyak.in">Павел Кузякин</a>
